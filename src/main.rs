788cd2508c6128b1b3981e6b170574725ea199b1
Lars Wigger <36078827+LarsWigger@users.noreply.github.com>
2022-03-21 11:46:46 +0100

    Refactored

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,53 @@
 fn main() {
-    let config = parser::parse(2, 2, 2, 2, 1);
+    parser::parse(2, 2, 2, 2, 1, 4);
     println!("Hello, world!");
 }
 
-mod data {
-    pub struct Config {
-        distance_matrix: Vec<u32>,
-        time_matrix: Vec<u32>,
-    }
-    impl Config {
-        pub fn new(distance_matrix: Vec<u32>, time_matrix: Vec<u32>) -> Config {
-            return Config {
-                distance_matrix: distance_matrix,
-                time_matrix: time_matrix,
-            };
-        }
-    }
-}
-
 mod parser {
-    use crate::data::Config;
     use std::fs;
     use std::path::Path;
 
-    pub struct SampleIdentifier {
-        full_pickup: u32,
-        empty_pickup: u32,
-        empty_delivery: u32,
-        afs: u32,
-        number: u32,
+    pub struct DataIdentifier {
+        full_pickup: usize,
+        empty_pickup: usize,
+        empty_delivery: usize,
+        afs: usize,
+        sample_number: usize,
+        scenario: usize,
+        num_trucks: usize,
+        t_max: usize,
     }
 
-    impl SampleIdentifier {
+    impl DataIdentifier {
         fn get_base_folder(&self) -> String {
             //how is this defined? unclear whether delivery also counts
-            let data_type = if self.full_pickup > self.empty_delivery {
-                "A"
-            } else {
-                "B"
-            };
             return format!(
                 "{}f{}p{}d{}s{}",
-                self.full_pickup, self.empty_pickup, self.empty_delivery, self.afs, data_type
+                self.full_pickup,
+                self.empty_pickup,
+                self.empty_delivery,
+                self.afs,
+                self.get_data_type()
             );
         }
-        fn get_matrix_size(&self) -> u32 {
-            return self.full_pickup + self.empty_pickup + self.empty_delivery + self.afs;
+        fn get_data_type(&self) -> &str {
+            if (self.full_pickup > self.empty_delivery) {
+                return "A";
+            } else {
+                return "B";
+            };
+        }
+        fn get_matrix_dimension(&self) -> usize {
+            return (2 * self.full_pickup) + self.empty_pickup + self.empty_delivery + self.afs + 2;
         }
         fn get_matrix_file_ending(&self) -> String {
             return format!(
                 "{}_F{}_P{}_D_{}S_No{}.txt",
-                self.full_pickup, self.empty_pickup, self.empty_delivery, self.afs, self.number
+                self.full_pickup,
+                self.empty_pickup,
+                self.empty_delivery,
+                self.afs,
+                self.sample_number
             );
         }
         fn get_distance_matrix_file_name(&self) -> String {
@@ -59,48 +56,58 @@ mod parser {
         fn get_time_matrix_file_name(&self) -> String {
             return format!("TimeMatrix{}", self.get_matrix_file_ending());
         }
+        fn get_fuel_file_name(&self) -> String {
+            return format!("FuelLevel{}_T.txt", self.num_trucks);
+        }
+        fn get_total_capacity_file_name(&self) -> String {
+            return format!("Truck{}_T.txt", self.num_trucks);
+        }
+        fn get_resouce_file_name(&self) -> String {
+            return format!("Resource{}_T.txt", self.num_trucks);
+        }
+        fn get_base_file_name(&self) -> String {
+            return format!(
+                "{}f{}p{}d{}s{}n{}.txt",
+                self.full_pickup,
+                self.empty_pickup,
+                self.empty_delivery,
+                self.afs,
+                self.sample_number,
+                self.get_data_type()
+            );
+        }
+        fn parse_num_trucks_and_t_max(&mut self, base_path: &Path) {
+            let path = base_path.join(self.get_base_file_name());
+            println!("Parsing {} ...", path.display());
+            let file_string = fs::read_to_string(path).expect("Unable to parse file");
+            let mut lines = file_string.lines();
+            //remove header and the lines before the intended one
+            for _ in 0..self.scenario {
+                _ = lines.next()
+            }
+            let mut entries = lines.next().unwrap().split_whitespace();
+            for i in 0..10 {
+                if (i == 2) {
+                    //num_trucks
+                    self.num_trucks = entries.next().unwrap().parse().unwrap();
+                } else if (i == 7) {
+                    self.t_max = entries.next().unwrap().parse().unwrap();
+                } else {
+                    _ = entries.next();
+                }
+            }
+        }
     }
 
     const BASE_PATH_STR: &str = "C:\\Users\\larsw\\Documents\\Workspaces\\Dial-A-Ride\\data";
 
     pub fn parse(
-        full_pickup: u32,
-        empty_pickup: u32,
-        empty_delivery: u32,
-        afs: u32,
-        number: u32,
-    ) -> Config {
-        let sample = SampleIdentifier {
-            full_pickup: full_pickup,
-            empty_pickup: empty_pickup,
-            empty_delivery: empty_delivery,
-            afs: afs,
-            number: number,
-        };
-        //calculate base_path
-        let base_path = Path::new(BASE_PATH_STR);
-        let base_path = base_path.join(sample.get_base_folder());
-        //parse matrices
-        let matrix_size = (sample.get_matrix_size() ^ 2) as usize;
-        let mut distance_matrix = Vec::with_capacity(matrix_size);
-        parse_matrix(
-            &mut distance_matrix,
-            &base_path.join(sample.get_distance_matrix_file_name()),
-        );
-        let mut time_matrix = Vec::with_capacity(matrix_size);
-        parse_matrix(
-            &mut time_matrix,
-            &base_path.join(sample.get_time_matrix_file_name()),
-        );
-        return crate::data::Config::new(distance_matrix, time_matrix);
-    }
-
-    fn parse_matrix(matrix: &mut Vec<u32>, path: &Path) {
-        //read file
-        println!("Parsing matrix from {} ...", path.display());
-        let file_string = fs::read_to_string(path).expect("Unable to parse matrix");
-        for number in file_string.split_whitespace() {
-            matrix.push(number.parse().unwrap());
-        }
+        full_pickup: usize,
+        empty_pickup: usize,
+        empty_delivery: usize,
+        afs: usize,
+        sample_number: usize,
+        scenario: usize,
+    ) {
     }
 }
