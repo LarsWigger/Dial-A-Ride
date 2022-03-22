0076f69d3be38cd4ce009b9fbb968ff630d009b1
Lars Wigger <36078827+LarsWigger@users.noreply.github.com>
2022-03-22 18:26:07 +0100

    Implemented basic path manipulation

--- src/solver.rs
+++ src/solver.rs
@@ -32,26 +32,48 @@ mod solver_data {
     }
 
     impl PathOption {
-        pub fn new(
+        ///Creates a new path option that is reached from the state described in `config` and `previous_option`
+        /// (as well as its index, which needs to be passed separately) to the node `to`. If this is not possible due to little fuel, `None` is returned.
+        pub fn next_path_option(
             config: &Config,
             previous_option: &PathOption,
             previous_index: usize,
             to: usize,
-        ) -> PathOption {
+        ) -> Option<PathOption> {
             let from = previous_option.path[previous_option.path.len()];
             let fuel_needed = config.fuel_needed_for_route(from, to);
+            let fuel_level = previous_option.fuel_level - fuel_needed;
+            if fuel_level <= 0.0 {
+                return None;
+            }
             let total_distance =
                 previous_option.total_distance + config.get_distance_between(from, to);
             let total_time = previous_option.total_time + config.get_time_between(from, to);
             let mut path = Vec::with_capacity(2);
             path.push(to);
-            return PathOption {
-                fuel_level: previous_option.fuel_level - fuel_needed,
+            return Some(PathOption {
+                fuel_level,
                 total_distance,
                 total_time,
                 path,
                 previous_index,
-            };
+            });
+        }
+
+        ///If `other` is at the same node and none of its `fuel_level`, `total_distance` or `total_time` is better than that of `self`, true is returned, otherwise false
+        pub fn completely_superior_to(&self, other: &PathOption) -> bool {
+            return (self.fuel_level >= other.fuel_level)
+                && (self.total_distance <= other.total_distance)
+                && (self.total_time <= other.total_time)
+                && (self.path[self.path.len() - 1] == other.path[other.path.len() - 1]);
+        }
+
+        ///If `other` is at the same node and one of its `fuel_level`, `total_distance` or `total_time` is better than that of `self`, true is returned, otherwise false
+        pub fn partly_superior_to(&self, other: &PathOption) -> bool {
+            return (self.path[self.path.len() - 1] == other.path[other.path.len() - 1])
+                && ((self.fuel_level < other.fuel_level)
+                    || (self.total_distance > other.total_distance)
+                    || (self.total_time > other.total_time));
         }
     }
 
@@ -107,13 +129,39 @@ mod solver_data {
         pub fn route_to_node(&self, config: &Config, node: usize) -> SearchState {
             let mut path_options = Vec::with_capacity(4);
             let fuel_needed_directly = config.fuel_needed_for_route(self.current_node, node);
+            //first step, use previous ones to go directly to node
             for (index, option) in self.path_options.iter().enumerate() {
-                let option = &self.path_options[index];
-                if option.fuel_level > fuel_needed_directly {
-                    path_options.push(PathOption::new(config, &option, index, node));
-                }
+                let new_option = PathOption::next_path_option(config, option, index, node);
+                SearchState::possibly_add_to_path_options(&mut path_options, new_option);
             }
             panic!("Unimplemented");
+        }
+
+        fn possibly_add_to_path_options(
+            path_options: &mut Vec<PathOption>,
+            new_option: Option<PathOption>,
+        ) {
+            let unpacked_option = match new_option {
+                None => return,
+                Some(item) => item,
+            };
+            //to detect whether something was removed
+            let original_length = path_options.len();
+            //remove the entries that are completely inferior to the new one (CAN THIS EVEN HAPPEN?)
+            path_options.retain(|x| !unpacked_option.completely_superior_to(&x));
+            if (original_length != path_options.len()) {
+                //something was removed
+                path_options.push(unpacked_option);
+                return;
+            } else {
+                //check whether unpacked_option is at least partially superior to one of the existing ones
+                for i in 0..path_options.len() {
+                    if unpacked_option.partly_superior_to(&path_options[i]) {
+                        path_options.push(unpacked_option);
+                        return;
+                    }
+                }
+            }
         }
     }
 }
