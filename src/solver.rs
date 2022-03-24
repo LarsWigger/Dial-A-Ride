91580e9e3c4db10261e804234ff594d0704d3292
Lars Wigger <36078827+LarsWigger@users.noreply.github.com>
2022-03-24 12:16:59 +0100

    Progress backup

--- src/solver.rs
+++ src/solver.rs
@@ -1,12 +1,34 @@
 use crate::data::Config;
-use crate::data::ContainerRequest;
 use crate::data::Truck;
 
 mod solver_data {
     use crate::data::Config;
-    use crate::data::ContainerType;
+    use crate::data::ContainerRequest;
     use crate::data::Truck;
     use std::collections::HashMap;
+
+    #[derive(Eq, PartialEq)]
+    enum ContainerType {
+        ///a full 20 foot container for the specific full container pickup request
+        Full20(usize),
+        ///any empty 20 foot container, which one makes no difference
+        Empty20,
+        ///a full 40 foot container for the specific full container pickup request
+        Full40(usize),
+        ///an empty 40 foot container, which one makes no difference
+        Empty40,
+        ///no container at all
+        NoContainer,
+    }
+
+    impl ContainerType {
+        pub fn isNoContainer(&self) -> bool {
+            return match self {
+                NoContainer => true,
+                _ => false,
+            };
+        }
+    }
 
     struct Route {
         route: Vec<usize>,
@@ -75,11 +97,15 @@ mod solver_data {
                     || (self.total_distance > other.total_distance)
                     || (self.total_time > other.total_time));
         }
+
+        pub fn get_current_node(&self) -> usize {
+            return self.path[self.path.len() - 1];
+        }
     }
 
-    enum PreviousState {
-        NoPrevious,
-        Previous(Box<SearchState>),
+    enum NextState {
+        NoNext,
+        Next(Box<SearchState>),
     }
 
     pub struct SearchState {
@@ -89,7 +115,7 @@ mod solver_data {
         container_1: ContainerType,
         container_2: ContainerType,
         requests_visisted: u64,
-        previous_state: PreviousState,
+        next_state: NextState,
     }
 
     impl SearchState {
@@ -108,9 +134,103 @@ mod solver_data {
                 container_1: ContainerType::NoContainer,
                 container_2: ContainerType::NoContainer,
                 requests_visisted: 0,
-                previous_state: PreviousState::NoPrevious,
+                next_state: NextState::NoNext,
             };
             return search_state;
+        }
+
+        fn create_next_one_changing_containers(
+            &self,
+            config: &Config,
+            path_options: Vec<PathOption>,
+        ) -> &SearchState {
+            //this function should only be called when at least one path exists
+            let current_node = path_options[0].get_current_node();
+            let mut new_state = SearchState {
+                current_node,
+                path_options,
+                container_1: self.container_1,
+                container_2: self.container_2,
+                requests_visisted: self.requests_visisted,
+                next_state: NextState::NoNext,
+            };
+            //do containers need to be changed/is the new node a request?
+            if current_node < config.get_first_afs() {
+                new_state.handle_request_containers(config);
+            }
+            self.next_state = NextState::Next(Box::new(new_state));
+            return &new_state;
+        }
+
+        ///Adjusts the containers at the request node the state is currently at and marks this request as visited afterwards
+        /// Must only be called when the current node is a request (unchecked) and the request has not been visited before (checked)
+        fn handle_request_containers(&mut self, config: &Config) {
+            //request no already handled:
+            assert!(!self.get_request_visited(self.current_node));
+            let request = config.get_request_at_node(self.current_node);
+            //handle deload first in case load and deload happen at the same node (should not happen)
+            if request.full_20 < 0 {
+                let unloaded_container = ContainerType::Empty20;
+                self.unload_container(unloaded_container, request.full_20);
+            }
+            if request.full_40 < 0 {
+                let unloaded_container = ContainerType::Empty20;
+                self.unload_container(unloaded_container, request.full_20);
+            }
+            if request.empty_20 < 0 {
+                let unloaded_container = ContainerType::Empty20;
+                self.unload_container(unloaded_container, request.full_20);
+            }
+            if request.empty_40 < 0 {
+                let unloaded_container = ContainerType::Empty20;
+                self.unload_container(unloaded_container, request.full_20);
+            }
+            //handle load
+            if request.full_20 > 0 {
+                let container = ContainerType::Full20(self.current_node);
+                self.load_container(container, request.full_20);
+            }
+            if request.full_40 > 0 {
+                let container = ContainerType::Full40(self.current_node);
+                self.load_container(container, request.full_40);
+            }
+            if request.empty_20 > 0 {
+                let container = ContainerType::Empty20;
+                self.load_container(container, request.empty_20);
+            }
+            if request.empty_40 > 0 {
+                let container = ContainerType::Empty40;
+                self.load_container(container, request.empty_40);
+            }
+            self.set_request_visited(self.current_node);
+        }
+
+        ///Unloads `-number` (it is negative, the inversion is done in the method) containers of type `unload_container`
+        fn unload_container(&mut self, unload_container: ContainerType, number: i32) {
+            assert!(number < 0);
+            for _ in 0..-number {
+                if self.container_1 == unload_container {
+                    self.container_1 = ContainerType::NoContainer;
+                } else if self.container_2 == unload_container {
+                    self.container_2 = ContainerType::NoContainer;
+                } else {
+                    panic!("INVALID STATE");
+                }
+            }
+        }
+
+        ///Loads `number` containers of type `load_container`
+        fn load_container(&mut self, load_container: ContainerType, number: i32) {
+            assert!(number > 0);
+            for _ in 0..number {
+                if self.container_1.isNoContainer() {
+                    self.container_1 = load_container;
+                } else if self.container_2.isNoContainer() {
+                    self.container_2 = load_container;
+                } else {
+                    panic!("INVALID STATE");
+                }
+            }
         }
 
         pub fn get_request_visited(&self, request: usize) -> bool {
@@ -120,13 +240,13 @@ mod solver_data {
             return request_result != 0;
         }
 
-        pub fn set_request_visisted(&mut self, request: usize) {
+        fn set_request_visited(&mut self, request: usize) {
             //request binary is all 0 with 1 at the request index from the right
             let request_binary: u64 = 1 << request;
             self.requests_visisted = self.requests_visisted | request_binary;
         }
 
-        pub fn route_to_node(&self, config: &Config, node: usize) -> SearchState {
+        pub fn route_to_node(&self, config: &Config, node: usize) -> Option<&SearchState> {
             let mut path_options = Vec::with_capacity(4);
             let fuel_needed_directly = config.fuel_needed_for_route(self.current_node, node);
             //first step, use previous ones to go directly to node
@@ -134,15 +254,70 @@ mod solver_data {
                 let new_option = PathOption::next_path_option(config, option, index, node);
                 SearchState::possibly_add_to_path_options(&mut path_options, new_option);
             }
-            panic!("Unimplemented");
+            //second step, go to AFS in order to try reaching the node from there (possibly even chaining multiple AFS)
+            for (index, option) in self.path_options.iter().enumerate() {
+                for afs in config.get_first_afs()..config.get_first_afs() + config.get_afs() {
+                    let new_option = PathOption::next_path_option(config, option, index, afs);
+                    SearchState::possibly_add_to_path_options(&mut path_options, new_option);
+                }
+                //TODO: DEAL WITH REFUELING AT DEPOT
+            }
+            //third step, try using previous one to get to
+            let mut improvement_found = true;
+            while improvement_found {
+                improvement_found = false;
+                //to new node
+                for (index, option) in self.path_options.iter().enumerate() {
+                    //save for repeated usage:
+                    let current_node = option.get_current_node();
+                    //to new node
+                    if option.get_current_node() != node {
+                        let new_option = PathOption::next_path_option(config, option, index, node);
+                        let made_change = SearchState::possibly_add_to_path_options(
+                            &mut path_options,
+                            new_option,
+                        );
+                        if made_change {
+                            improvement_found = true;
+                        }
+                    }
+                    //to afs
+                    for afs in config.get_first_afs()..config.get_first_afs() + config.get_afs() {
+                        if current_node != afs {
+                            let new_option =
+                                PathOption::next_path_option(config, option, index, node);
+                            let made_change = SearchState::possibly_add_to_path_options(
+                                &mut path_options,
+                                new_option,
+                            );
+                            if made_change {
+                                improvement_found = true;
+                            }
+                        }
+                    }
+                    //TODO: DEAL WITH REFUELING AT DEPOT
+                }
+            }
+            //fourth step, remove anything that does not end at the depot
+            path_options.retain(|x| x.get_current_node() == node);
+            if path_options.len() == 0 {
+                //no path found
+                return None;
+            } else {
+                let result = self.create_next_one_changing_containers(config, path_options);
+                return Some(result);
+            }
         }
 
+        ///Removes the elements of `path_options` that are completely inferior to `new_option`
+        /// and adds `new_option` if it was partially superior to at least one of the previous elements
+        /// Returns `true` if a change was made
         fn possibly_add_to_path_options(
             path_options: &mut Vec<PathOption>,
             new_option: Option<PathOption>,
-        ) {
+        ) -> bool {
             let unpacked_option = match new_option {
-                None => return,
+                None => return false,
                 Some(item) => item,
             };
             //to detect whether something was removed
@@ -152,15 +327,16 @@ mod solver_data {
             if (original_length != path_options.len()) {
                 //something was removed
                 path_options.push(unpacked_option);
-                return;
+                return true;
             } else {
                 //check whether unpacked_option is at least partially superior to one of the existing ones
                 for i in 0..path_options.len() {
                     if unpacked_option.partly_superior_to(&path_options[i]) {
                         path_options.push(unpacked_option);
-                        return;
+                        return true;
                     }
                 }
+                return false;
             }
         }
     }
