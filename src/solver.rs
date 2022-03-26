faad8dbe295daf4251275c1a80c90d5788ef0fed
Lars Wigger <36078827+LarsWigger@users.noreply.github.com>
2022-03-26 17:18:50 +0100

    Improvements

--- src/solver.rs
+++ src/solver.rs
@@ -235,6 +235,7 @@ mod solver_data {
             truck: &Truck,
             previous_index: usize,
             to: usize,
+            depot_refuel: bool,
         ) -> Option<PathOption> {
             let from = self.path[self.path.len() - 1];
             let total_distance = config.get_distance_between(from, to);
@@ -247,19 +248,19 @@ mod solver_data {
             let mut total_time = self.total_time + config.get_time_between(from, to);
             //request handling times
             if to < config.get_first_afs() {
-                //can request still be processed?
-                if total_time > config.get_latest_visiting_time(to) {
-                    return None;
-                } else if total_time < config.get_earliest_visiting_time(to) {
-                    //have to wait
-                    total_time = config.get_earliest_visiting_time(to)
+                if to != 0 {
+                    //can request still be processed?
+                    if total_time > config.get_latest_visiting_time(to) {
+                        return None;
+                    } else if total_time < config.get_earliest_visiting_time(to) {
+                        //have to wait
+                        total_time = config.get_earliest_visiting_time(to)
+                    }
+                    total_time += config.get_service_time(to);
+                } else {
+                    //depot handling times
+                    //TODO!
                 }
-                total_time += config.get_service_time(to);
-            }
-            //refueling
-            if config.get_first_afs() <= to && to < config.get_dummy_depot() {
-                total_time += truck.get_minutes_for_refueling(fuel_level);
-                fuel_level = truck.get_fuel();
             }
             //create new path
             let new_path_len = self.path.len() + 1;
@@ -268,13 +269,21 @@ mod solver_data {
                 path.push(self.path[i]);
             }
             path.push(to);
-            return Some(PathOption {
+            //create new option before
+            let mut new_option = PathOption {
                 fuel_level,
                 total_distance,
                 total_time,
                 path,
                 previous_index,
-            });
+            };
+            //refuel if at AFS or at the depot if this is intended
+            if config.get_first_afs() <= to && to < config.get_dummy_depot()
+                || (to == 0 && depot_refuel)
+            {
+                new_option.refuel(truck);
+            }
+            return Some(new_option);
         }
 
         ///If `other` is at the same node and none of its `fuel_level`, `total_distance` or `total_time` is better than that of `self`, `true` is returned, otherwise `false`
@@ -293,8 +302,14 @@ mod solver_data {
                     || (self.total_time > other.total_time));
         }
 
+        ///Returns the node this `PathOption` is currently at
         pub fn get_current_node(&self) -> usize {
             return self.path[self.path.len() - 1];
+        }
+
+        pub fn refuel(&mut self, truck: &Truck) {
+            self.total_time += truck.get_minutes_for_refueling(self.fuel_level);
+            self.fuel_level = truck.get_fuel();
         }
     }
 
@@ -304,6 +319,7 @@ mod solver_data {
         current_node: usize,
         ///all the paths leading from the previous `SearchState` to `current_node`, sorted
         path_options: Vec<PathOption>,
+        ///represents the containers the truck is carrying in this state
         container_data: ContainerData,
         ///the requests that have been visited so far, binary encoding for efficiency
         requests_visisted: u64,
@@ -346,7 +362,7 @@ mod solver_data {
             let mut new_state = SearchState {
                 current_node,
                 path_options,
-                container_data: ContainerData::new(),
+                container_data: current_state.container_data.clone(),
                 requests_visisted: current_state.requests_visisted,
                 previous_state: Option::Some(new_state_reference),
             };
@@ -367,10 +383,10 @@ mod solver_data {
                 //full pickup request
                 self.container_data.num_20 += request.full_20;
                 self.container_data.num_40 += request.full_40;
-                if self.container_data.full_requests[0] == 0 {
-                    self.container_data.full_requests[0] = self.current_node;
-                } else if self.container_data.full_requests[1] == 0 {
-                    self.container_data.full_requests[1] = self.current_node;
+                if self.container_data.full_request_1_source == 0 {
+                    self.container_data.full_request_1_source = self.current_node;
+                } else if self.container_data.full_request_2_source == 0 {
+                    self.container_data.full_request_2_source = self.current_node;
                 } else {
                     panic!("THIS SHOULD NOT HAPPEN!");
                 }
@@ -384,13 +400,13 @@ mod solver_data {
             {
                 //full delivery
                 let source_node = config.get_pick_node_for_full_dropoff(self.current_node);
-                let request = config.get_request_at_node(source_node);
-                self.container_data.num_20 -= request.full_20;
-                self.container_data.num_40 -= request.full_40;
-                if self.container_data.full_requests[0] == source_node {
-                    self.container_data.full_requests[0] = 0;
-                } else if self.container_data.full_requests[1] == source_node {
-                    self.container_data.full_requests[1] = 0;
+                //values are negative, so this is effectively a subtraction
+                self.container_data.num_20 += request.full_20;
+                self.container_data.num_40 += request.full_40;
+                if self.container_data.full_request_1_source == source_node {
+                    self.container_data.full_request_1_source = 0;
+                } else if self.container_data.full_request_2_source == source_node {
+                    self.container_data.full_request_2_source = 0;
                 } else {
                     panic!("THIS SHOULD NOT HAPPEN!");
                 }
@@ -406,25 +422,23 @@ mod solver_data {
         }
 
         ///Returns whether the given `request` has been visited, makes the binary encoding more accessible.
-        /// Indexing starts at 1 so it is consistent with the nodes (asserted)
         pub fn get_request_visited(&self, request_node: usize) -> bool {
             assert!(request_node != 0 && request_node < 64);
             //request binary is all 0 with 1 at the request index from the right
-            let request_binary: u64 = 1 << request_node - 1;
+            let request_binary: u64 = 1 << (request_node - 1);
             let request_result = self.requests_visisted & request_binary;
             return request_result != 0;
         }
 
         ///Sets the given `request` as visited, makes the binary encoding more accessible
-        ///Indexing starts at 1 so it is consistent with the nodes (asserted)
         fn set_request_visited(&mut self, request_node: usize) {
             assert!(request_node != 0 && request_node < 64);
             //request binary is all 0 with 1 at the request index from the right
-            let request_binary: u64 = 1 << request_node - 1;
+            let request_binary: u64 = 1 << (request_node - 1);
             self.requests_visisted = self.requests_visisted | request_binary;
         }
 
-        /// Calculates all the relevant routes from `current_state` to the target node and returns the next `SearchState` alreay wrapped in `Rc`
+        /// Calculates all the relevant routes from `current_state` to the target node and returns the next `SearchState` already wrapped in `Rc`
         /// This may be `Option::None` if no route is found
         pub fn route_to_node(
             config: &Config,
@@ -432,30 +446,33 @@ mod solver_data {
             current_state: &Rc<SearchState>,
             node: usize,
         ) -> Option<Rc<SearchState>> {
-            let mut path_options = Vec::with_capacity(4);
-            //first step, use previous ones to go directly to node
+            assert!(current_state.current_node != node);
+            //it is not predictable in advance how big this will get, so I just initialize it with 8 (probably bad)
+            let mut path_options = Vec::with_capacity(8);
+            //first step, use previous ones to go directly to node or fuel stations or depot
             for (index, option) in current_state.path_options.iter().enumerate() {
-                let new_option = option.next_path_option(config, truck, index, node);
+                //target node
+                let new_option = option.next_path_option(config, truck, index, node, false);
                 SearchState::possibly_add_to_path_options(&mut path_options, new_option);
-            }
-            //second step, go to AFS in order to try reaching the node from there (possibly even chaining multiple AFS)
-            for (index, option) in current_state.path_options.iter().enumerate() {
+                //fuel stations
                 for afs in config.get_first_afs()..config.get_first_afs() + config.get_afs() {
-                    let new_option = option.next_path_option(config, truck, index, afs);
+                    let new_option = option.next_path_option(config, truck, index, afs, false);
                     SearchState::possibly_add_to_path_options(&mut path_options, new_option);
                 }
-                //TODO: DEAL WITH REFUELING AT DEPOT
+                //depot (only for refueling, pass true to the parameter)
+                let new_option = option.next_path_option(config, truck, index, node, true);
+                SearchState::possibly_add_to_path_options(&mut path_options, new_option);
             }
-            //third step, try using previously found path_options one to find a better path to somewhere (not only the depot)
+            //second step, try using previously found path_options one to find a better path to somewhere (not only the depot)
             let mut improvement_found = true;
             while improvement_found {
                 improvement_found = false;
                 for (index, option) in current_state.path_options.iter().enumerate() {
                     //save for repeated usage:
                     let current_node = option.get_current_node();
-                    //to new node
+                    //to target node
                     if option.get_current_node() != node {
-                        let new_option = option.next_path_option(config, truck, index, node);
+                        let new_option = option.next_path_option(config, truck, index, node, false);
                         let made_change = SearchState::possibly_add_to_path_options(
                             &mut path_options,
                             new_option,
@@ -467,12 +484,13 @@ mod solver_data {
                     //refueling, makes no sense starting from the target node
                     if current_node != node {
                         //to afs
-                        for afs in config.get_first_afs()..config.get_first_afs() + config.get_afs()
+                        for afs in
+                            config.get_first_afs()..(config.get_first_afs() + config.get_afs())
                         {
                             //routing from itself to itself is completely pointless
                             if current_node != afs {
                                 let new_option =
-                                    option.next_path_option(config, truck, index, node);
+                                    option.next_path_option(config, truck, index, node, false);
                                 let made_change = SearchState::possibly_add_to_path_options(
                                     &mut path_options,
                                     new_option,
@@ -482,14 +500,23 @@ mod solver_data {
                                 }
                             }
                         }
-                        //TODO: DEAL WITH REFUELING AT DEPOT
+                        //to depot
+                        //this may be called when already at the depot. In that case, depot will appear twice in the path, but the result will be refueled
+                        let new_option = option.next_path_option(config, truck, index, node, true);
+                        let made_change = SearchState::possibly_add_to_path_options(
+                            &mut path_options,
+                            new_option,
+                        );
+                        if made_change {
+                            improvement_found = true;
+                        }
                     }
                 }
             }
-            //fourth step, remove anything that does not end at the depot
+            //third step, remove anything that does not end at the depot
             path_options.retain(|x| x.get_current_node() == node);
             if path_options.len() == 0 {
-                //no path found, overwrite just in case
+                //no path found
                 return Option::None;
             } else {
                 return Option::Some(SearchState::create_next_state_after_current_state(
@@ -531,6 +558,7 @@ mod solver_data {
             }
         }
 
+        //Getter for `current_node`
         pub fn get_current_node(&self) -> usize {
             return self.current_node;
         }
@@ -548,12 +576,15 @@ mod solver_data {
             return (best_index, lowest_distance);
         }
 
+        ///Returns `true` if the state can handle the request next (without visiting a different request first)
         pub fn can_handle_request(
             &self,
             config: &Config,
             truck: &Truck,
             request_node: usize,
         ) -> bool {
+            assert!(self.container_data.num_20 <= truck.get_num_20_foot_containers());
+            assert!(self.container_data.num_40 <= truck.get_num_40_foot_containers());
             //TODO: change container system to only allow one change per pickup, validating it in the data
             //cannot visit the same request twice
             if self.get_request_visited(request_node) {
@@ -562,11 +593,10 @@ mod solver_data {
             let request = config.get_request_at_node(request_node);
             //only to need to check whether something can be picked up
             if request_node < config.get_first_full_dropoff() {
-                assert!(self.container_data.num_20 <= truck.get_num_20_foot_containers());
-                assert!(self.container_data.num_40 <= truck.get_num_40_foot_containers());
+                //pickup request
                 let newly_loaded_20 = request.empty_20 + request.full_20;
                 if newly_loaded_20 > 0 {
-                    if newly_loaded_20 + self.container_data.num_20
+                    if (newly_loaded_20 + self.container_data.num_20)
                         > truck.get_num_20_foot_containers()
                     {
                         return false;
@@ -582,10 +612,14 @@ mod solver_data {
                 }
                 assert!(newly_loaded_20 + newly_loaded_40 > 0);
             } else if request_node >= config.get_first_full_dropoff() {
+                //dropoff
                 if request_node >= config.get_first_full_dropoff() + config.get_full_pickup() {
+                    //full container dropoof
                     //just check whether the corresponging pickup request was visited
-                    self.get_request_visited(config.get_pick_node_for_full_dropoff(request_node));
+                    return self
+                        .get_request_visited(config.get_pick_node_for_full_dropoff(request_node));
                 } else {
+                    //empty container dropoff
                     if -request.empty_20 > self.container_data.empty_20 {
                         return false;
                     }
@@ -599,30 +633,39 @@ mod solver_data {
             return true;
         }
 
+        ///Get the `PathOption` at the given `index`
         fn get_path(&self, index: usize) -> &PathOption {
             return &self.path_options[index];
         }
     }
 
+    ///Combines all the data about the current state of loaded containers for a `SearchState`
+    #[derive(Copy, Clone)]
     struct ContainerData {
+        ///number of empty 20 foot containers loaded
         empty_20: i32,
+        ///number of empty 40 foot containers loaded
         empty_40: i32,
+        ///number of 20 foot containers (empty + full) loaded. Saving it this way saves computations
         num_20: i32,
+        ///number of 40 foot containers (empty + full) loaded. Saving it this way saves computations
         num_40: i32,
-        full_requests: Vec<usize>,
+        ///if a full container is loaded, its origin is saved here or in `full_request_2_source`. 0 indicates nothing saved at the moment
+        full_request_1_source: usize,
+        ///see `full_request_1_source`
+        full_request_2_source: usize,
     }
 
     impl ContainerData {
+        ///Creates a new `ContainerData` representing nothing currently carried
         pub fn new() -> ContainerData {
-            let mut full_requests = Vec::with_capacity(2);
-            full_requests.push(0);
-            full_requests.push(0);
             return ContainerData {
                 empty_20: 0,
                 empty_40: 0,
                 num_20: 0,
                 num_40: 0,
-                full_requests,
+                full_request_1_source: 0,
+                full_request_2_source: 0,
             };
         }
     }
@@ -667,11 +710,10 @@ fn solve_for_truck_recursive(
     known_options: &mut KnownRoutesForTruck,
     current_state: &Rc<SearchState>,
 ) {
-    if current_state.get_current_node() == 0 {
+    //routes can end only at the dummy depot
+    if current_state.get_current_node() == config.get_dummy_depot() {
         known_options.possibly_add(&current_state);
-    } else if current_state.get_current_node() == config.get_dummy_depot() {
-        known_options.possibly_add(&current_state);
-        return; //should never be left again
+        return;
     }
     //try moving to the requests
     for request_node in 1..config.get_first_afs() {
@@ -687,7 +729,7 @@ fn solve_for_truck_recursive(
         };
     }
     //try changing containers at depot
-    //optimization potential: do this at the beginning, if the depot cannot be reached AND the dummy depot can't be reached directly, this state is a dead end
+    //optimization potential: do this at the beginning, if the depot cannot be reached, this state is a dead end
     //if necessary, navigate to the depot
     let depot_state;
     if current_state.get_current_node() == 0 {
